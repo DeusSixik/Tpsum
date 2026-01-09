2e3f9f5e77b9442e136fea5dfa676fa85c4ca3a4
Bogdan Beloshkurenko <142807122+DeusSixik@users.noreply.github.com>
2026-01-09 11:53:24 +0300

    feat: Add MobCount Optimiztion

--- src/main/java/dev/sixik/tpsum/Tpsum.java
+++ src/main/java/dev/sixik/tpsum/Tpsum.java
@@ -1,7 +1,6 @@
 package dev.sixik.tpsum;
 
 import com.mojang.logging.LogUtils;
-import com.xiaohunao.heaven_destiny_moment.HeavenDestinyMoment;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
@@ -13,9 +12,6 @@ public class Tpsum {
     public static final String MODID = "tpsum";
     public static final Logger LOGGER = LogUtils.getLogger();
 
-    public Tpsum(IEventBus modEventBus, ModContainer modContainer) {
-
-
-    }
+    public Tpsum(IEventBus modEventBus, ModContainer modContainer) { }
 
 }
